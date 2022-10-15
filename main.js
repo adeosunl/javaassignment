cee80e509edcaabee82eeb49d8a81805ee82cba3
olumide <adeosunolumidefikayomi@gmail.com>
2022-10-15 11:17:43 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const pI = 3.14;
+
+document.getElementById("pI").innerHTML = pI;
+
+console.log(pI)
+
+
+let x = 22;
+let y = 7;
+let z = x / y;
+
+document.getElementById("demo").innerHTML = z;
+console.log(z)
+
+
+let countryName = ["Nigeria" , "England" , "Portugal" , "Spain"]
+
+document.getElementById("num2").innerHTML = countryName;
+
+console.log(countryName)
+
+
+let age = ["12" , "13" , "14" , "15"]
+
+document.getElementById("num3").innerHTML = age;
+
+console.log(age)
+
+const name1 = "Adeosun";
+const name2 = "olumide";
+const name3 = name1 + " " + name2;
+
+document.getElementById("num4").innerHTML = name3;
+
+console.log(name3)
+
+
